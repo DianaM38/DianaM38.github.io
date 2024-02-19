ab5429bfa1bbfce977fa8fb86673a613ab1cb8ff
Diana <130005158+DianaM38@users.noreply.github.com>
2024-02-19 11:07:27 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.travail');
+ScrollReveal().reveal('.news', { delay: 900 });
+ScrollReveal().reveal('.objetivos', { delay: 800 });
+ScrollReveal().reveal('.banner', { delay: 700 });
